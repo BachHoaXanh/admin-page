727bfbf0eac430b61a76bc17a89dc53d783c9133
lntthanh <lengoctienthanh@gmail.com>
2020-12-21 23:29:50 +0700

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
+const logger = new Logger('Bootstrap');
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+        cors: true,
+    });
+
+    app.enableCors();
+    app.setGlobalPrefix('/api');
+
+    await app.listen(port);
 }
-bootstrap();
+bootstrap().then(() => logger.log('Bach Hoa Xanh bootstrapped.'));
