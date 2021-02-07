5ec9a921970e4c8045a6eabd3eaaacd310d7c8d5
lntthanh <lengoctienthanh@gmail.com>
2021-02-08 00:09:17 +0700

    add axios & create user service

---
+++ client/src/services/users.service.js
@@ -0,0 +1,24 @@
+import http from '../http-common';
+
+class UsersService {
+  list() {
+    return http.get('users');
+  }
+
+  get(id) {
+    return http.get(`users/${id}`);
+  }
+
+  create(data) {
+    return http.post('users', data);
+  }
+
+  update(id, data) {
+    return http.put(`users/${id}`, data);
+  }
+
+  delete(id) {
+    return http.delete(`users/${id}`);
+  }
+
+}
