32a5c8031fbd03db954f27415c902730867bbe1e
Thanh Le <thanh.lengoctien@axonactive.com>
2021-02-17 10:54:52 +0700

    create category

--- client/src/views/managements/categories/Create.js
+++ client/src/views/managements/categories/Create.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {
   CButton,
   CCard,
@@ -10,14 +10,29 @@ import {
   CFormGroup,
   CFormText,
   CInput,
-  CInputFile,
   CLabel,
   CSelect,
-  CSwitch
-} from '@coreui/react'
+  } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
+import axios from "axios";
 
-const Create = () => {
+const Create = (props) => {
+  let name = useFormInput('');
+  let parent = useFormInput('');
+
+  const handleSubmit = () => {
+    axios.post('http://localhost:3000/api/categories', {
+      name: name.value,
+      parent: parseInt(parent.value.toString())
+    }).then(() => {
+      alert('Successfully');
+      props.history.push('/managements/categories');
+    }).catch((error) => {
+      alert('Something went wrong');
+      console.log(error);
+    });
+  };
+
   return (
     <>
       <CCol xs="12" md="12">
@@ -29,87 +44,42 @@ const Create = () => {
             <CForm action="" method="post" encType="multipart/form-data" className="form-horizontal">
               <CFormGroup row>
                 <CCol md="3">
-                  <CLabel htmlFor="text-input">First Name</CLabel>
+                  <CLabel htmlFor="text-input">Name</CLabel>
                 </CCol>
                 <CCol xs="12" md="9">
-                  <CInput id="first-name" name="text-input" placeholder="First Name"/>
-                  <CFormText>Please enter your first name</CFormText>
+                  <CInput id="first-name" name="text-input" placeholder="Name" {...name}/>
+                  <CFormText>Please enter Category name</CFormText>
                 </CCol>
               </CFormGroup>
+
               <CFormGroup row>
                 <CCol md="3">
-                  <CLabel htmlFor="text-input">Last Name</CLabel>
+                  <CLabel htmlFor="select">Parent</CLabel>
                 </CCol>
                 <CCol xs="12" md="9">
-                  <CInput id="last-name" name="text-input" placeholder="Last Name"/>
-                  <CFormText>Please enter your last name</CFormText>
-                </CCol>
-              </CFormGroup>
-              <CFormGroup row>
-                <CCol md="3">
-                  <CLabel htmlFor="email-input">Email</CLabel>
-                </CCol>
-                <CCol xs="12" md="9">
-                  <CInput type="email" id="email-input" name="email-input" placeholder="Email"
-                          autoComplete="email"/>
-                  <CFormText className="help-block">Please enter your email</CFormText>
-                </CCol>
-              </CFormGroup>
-              <CFormGroup row>
-                <CCol md="3">
-                  <CLabel htmlFor="password-input">Password</CLabel>
-                </CCol>
-                <CCol xs="12" md="9">
-                  <CInput type="password" id="password-input" name="password-input" placeholder="Password"
-                          autoComplete="new-password"/>
-                  <CFormText className="help-block">Please enter a complex password</CFormText>
-                </CCol>
-              </CFormGroup>
-              <CFormGroup row>
-                <CCol md="3">
-                  <CLabel htmlFor="select">Role</CLabel>
-                </CCol>
-                <CCol xs="12" md="9">
-                  <CSelect custom name="select" id="select">
-                    <option value="0">Please select role</option>
-                    <option value="admin">Admin</option>
-                    <option value="teacher">Teacher</option>
-                    <option value="student">Student</option>
+                  <CSelect custom name="select" id="select" {...parent}>
+                    <option value="0">Please select Category Parent</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
                   </CSelect>
-                </CCol>
-              </CFormGroup>
-              <CFormGroup row>
-                <CLabel col md="3" htmlFor="file-input">Avatar</CLabel>
-                <CCol xs="12" md="9">
-                  <CInputFile id="file-input" name="file-input"/>
-                </CCol>
-              </CFormGroup>
-              <CFormGroup row>
-                <CCol tag="label" sm="3" className="col-form-label">
-                  Active
-                </CCol>
-                <CCol sm="9">
-                  <CSwitch
-                    className="mr-1"
-                    color="dark"
-                    defaultChecked
-                    shape="pill"
-                    variant="opposite"
-                  />
                 </CCol>
               </CFormGroup>
             </CForm>
           </CCardBody>
           <CCardFooter>
-            <CButton type="submit" size="sm" color="primary" style={{marginRight: '1rem'}}>
+            <CButton type="submit" size="sm" color="primary" style={{marginRight: '1rem'}} onClick={handleSubmit}>
               <CIcon name="cil-scrubber"/> Submit</CButton>
-            <CButton type="reset" size="sm" color="danger">
-              <CIcon name="cil-ban"/> Reset</CButton>
           </CCardFooter>
         </CCard>
       </CCol>
     </>
   )
+}
+
+const useFormInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+  return { value, onChange: e => setValue(e.target.value) };
 }
 
 export default Create
